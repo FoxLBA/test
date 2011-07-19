564bc03987e7eb16f295fedbecaf6cba1ce73f1a
Vadim Gusev <ubuntolog@gmail.com>
2011-07-19 07:43:04 +0400

    resolve all file names, cleanup code

--- apps/test-multifile/test-multifile.cpp
+++ apps/test-multifile/test-multifile.cpp
@@ -3,20 +3,20 @@
 #include "filesys.h"
 #include "boinc_api.h"
 
-#include <unistd.h>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-
 #define INPUT_FILENAME "in"
 #define OUTPUT_FILENAME "out"
+#define INPUT_BACKGROUND "in_back"
+#define OUTPUT_BACKGROUND "out_back"
+#define CONFIG_FILENAME "config"
+#define LOG_FILENAME "log"
 
 
 int main(int argc, char **argv) {
     int retval;
-    char input_path[512], output_path[512], tmp_name[512]="tmp.avi", buf[65535];
-    char oldname[65535];
-    char newname[65535];
+    char buf[65535];
+    char input_path[512],   output_path[512];
+    char inputbg_path[512], outputbg_path[512];
+    char config_path[512],  log_path[512];
 
     retval = boinc_init();
     if (retval) {
@@ -25,23 +25,18 @@ int main(int argc, char **argv) {
         exit(retval);
     }
 
-    // resolve the input file name
-    boinc_resolve_filename(INPUT_FILENAME, input_path, sizeof(input_path));
-
-    // resolve the output file name
-    boinc_resolve_filename(OUTPUT_FILENAME, output_path, sizeof(output_path));
+    // resolve file names
+    boinc_resolve_filename(INPUT_FILENAME,      input_path,     sizeof(input_path));
+    boinc_resolve_filename(OUTPUT_FILENAME,     output_path,    sizeof(output_path));
+    boinc_resolve_filename(INPUT_BACKGROUND,    inputbg_path,   sizeof(inputbg_path));
+    boinc_resolve_filename(OUTPUT_BACKGROUND,   outputbg_path,  sizeof(outputbg_path));
+    boinc_resolve_filename(CONFIG_FILENAME,     config_path,    sizeof(config_path));
+    boinc_resolve_filename(LOG_FILENAME,        log_path,       sizeof(log_path));
 
     // наложение фильтра    ffmpeg -vf drawbox=400:400:250:250:red -i %s %s_copy.avi
-    sprintf(buf, "mencoder -vf rectangle=400:400:250:250:red -o %s_copy.avi -oac copy -ovc lavc %s", input_path, input_path);
+    sprintf(buf, "mencoder -vf rectangle=400:400:250:250:red -o %s -oac copy -ovc lavc %s", output_path, input_path);
     fprintf(stderr, "command: %s\n", buf);
     system(buf);
-
-    sprintf(oldname, "%s_copy.avi", input_path);
-    sprintf(newname, "%s", output_path);
-
-    rename(oldname, newname);
-
-//    boinc_rename(tmp_name, output_path);
 
     //cleanup and exit
     boinc_fraction_done(1);
