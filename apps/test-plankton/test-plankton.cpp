7ffa366c230b86127ee35550679e838979031b6e
boincadm <boincadm@5k106-main.(none)>
2011-08-07 17:43:48 +0400

    add decrypt() to app (not tested), update makefile

--- apps/test-plankton/test-plankton.cpp
+++ apps/test-plankton/test-plankton.cpp
@@ -3,6 +3,8 @@
 #include "filesys.h"
 #include "boinc_api.h"
 
+#include <tomcrypt.h>
+
 #define INPUT_FILENAME "in"
 #define OUTPUT_FILENAME "out"
 #define INPUT_BACKGROUND "in_back"
@@ -10,6 +12,56 @@
 #define CONFIG_FILENAME "config"
 #define LOG_FILENAME "log"
 
+int decrypt(char *infile) {
+    unsigned char key[16]="qwertyuiopasdfg", IV[16]="1234567890ABCDE", buffer[512];
+    symmetric_CTR ctr;
+    int err, numbytes;
+    int retval;
+
+    FILE* input = fopen(infile, "rb");
+    if (input == NULL) {printf("error on input file\n"); exit (1);}
+    FILE* output = fopen("tmp", "wb");
+    if (output == NULL) {printf("error on output file\n"); exit (1);}
+
+    if (register_cipher (&twofish_desc) == -1) {
+        printf("Error registering cipher\n");
+        return -1;
+    }
+    // Decrypt
+    if ((err = ctr_setiv(IV,    /* the initial IV we gave to ctr_start */
+                       16,      /* the IV is 16 bytes long */
+                       &ctr)    /* the ctr state we wish to modify */
+    )!= CRYPT_OK) {
+        printf("ctr_setiv error: %s\n", error_to_string (err));
+        return -1;
+    }
+
+    while (numbytes = fread(buffer, 1, sizeof(buffer), input)) {
+        if ((err = ctr_decrypt (buffer, buffer, sizeof(buffer), &ctr)) != CRYPT_OK) {
+            printf("ctr_decrypt error: %s\n", error_to_string (err));
+            return -1;
+        }
+        fwrite(buffer, 1, numbytes, output);
+    }
+
+    /* terminate the stream */
+    if ((err = ctr_done (&ctr)) != CRYPT_OK) {
+        printf("ctr_done error: %s\n", error_to_string (err));
+        return -1;
+    }
+    // rename
+    retval = boinc_rename("tmp", infile);
+    if (retval) {
+        printf("Error [%d] renaming file cipher.bin to %s\n", retval, infile);
+    }
+    /* clear up and return */
+    fclose(input);
+    fclose(output);
+    zeromem(key, sizeof (key));
+    zeromem(&ctr, sizeof (ctr));
+
+    return 0;
+}
 
 int main(int argc, char **argv) {
     int retval;
@@ -36,6 +88,9 @@ int main(int argc, char **argv) {
     // assume we did something with those files
     boinc_copy(inbg_path, outbg_path);
     boinc_copy(config_path, log_path);
+
+    // decrypt
+    decrypt(input_path);
 
     // наложение фильтра    ffmpeg -vf drawbox=400:400:250:250:red -i %s %s_copy.avi
     sprintf(buf, "mencoder -vf rectangle=400:400:250:250:red -o %s -oac copy -ovc lavc %s", output_path, input_path);
