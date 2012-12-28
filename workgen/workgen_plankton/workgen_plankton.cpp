82a4496f45f1a152c404d3fb3ed515d11b0dbbf6
Vadim Gusev <ubuntolog@gmail.com>
2012-12-28 13:14:37 +0400

    increase workunit disk bound 10 GB => 20 GB

--- workgen/workgen_plankton/workgen_plankton.cpp
+++ workgen/workgen_plankton/workgen_plankton.cpp
@@ -223,7 +223,7 @@ int make_job(char* db_taskID) {
     wu.rsc_fpops_est = 1e12;
     wu.rsc_fpops_bound = 1e14;
     wu.rsc_memory_bound = 1e8;
-    wu.rsc_disk_bound = 1e10;
+    wu.rsc_disk_bound = 2*1e10;
     wu.delay_bound = 86400;
     wu.min_quorum = REPLICATION_FACTOR;
     wu.target_nresults = REPLICATION_FACTOR;
